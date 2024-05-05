eecf4ece92fa047dfb34a57cf2465d8963bcbaa6
Sönke Ohls <S@Ohls.eu>
2024-05-05 16:49:26 +0200

    Kommentare hübsch gemacht

--- 01-blinky-builtin/01-blinky-builtin.ino
+++ 01-blinky-builtin/01-blinky-builtin.ino
@@ -1,10 +1,10 @@
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);  // Benutzt Macro LED_BUILTIN um den GPIO2 als Ausgang definieren
+  pinMode(LED_BUILTIN, OUTPUT);        // Benutzt Macro LED_BUILTIN um  GPIO2 als Ausgang zu definieren
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // Schaltet die LED an
-  delay(1000);              // Wartet 1000 Millisekunden (1 Sekunde)
-  digitalWrite(LED_BUILTIN, LOW);    // Schaltet die LED aus
-  delay(1000);              // Wartet weitere 1000 Millisekunden
+  digitalWrite(LED_BUILTIN, HIGH);     // Schaltet die LED an
+  delay(1000);                         // Wartet 1000 Millisekunden (1 Sekunde)
+  digitalWrite(LED_BUILTIN, LOW);      // Schaltet die LED aus
+  delay(1000);                         // Wartet weitere 1000 Millisekunden
 }
--- 02-blinky-led/02-blinky-led.ino
+++ 02-blinky-led/02-blinky-led.ino
@@ -1,12 +1,14 @@
 #define LED_PIN 2
 
+
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);            // Benutzt Macro LED_BUILTIN um GPIO2 als Ausgang zu definieren
 }
 
+
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
-  delay(1000);
+  digitalWrite(LED_PIN, HIGH);         // Schaltet die LED an
+  delay(1000);                         // Wartet 1000 Millisekunden (1 Sekunde)
+  digitalWrite(LED_PIN, LOW);          // Schaltet die LED aus
+  delay(1000);                         // Wartet weitere 1000 Millisekunden
 }
